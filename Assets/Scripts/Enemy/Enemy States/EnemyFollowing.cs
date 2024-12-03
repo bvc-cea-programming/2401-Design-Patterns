676b8e11c6397334d8d12b263350649dcaa50b92
huoxinghua <68455980+huoxinghua@users.noreply.github.com>
2024-12-02 19:33:10 -0700

    first finish task

--- Assets/Scripts/Enemy/Enemy States/EnemyFollowing.cs
+++ Assets/Scripts/Enemy/Enemy States/EnemyFollowing.cs
@@ -28,10 +28,12 @@ public class EnemyFollowing : BaseState
         {
             _stateMachine.EnemyMovement.StartMovement();
             _stateMachine.EnemyMovement.FollowTarget(_threat);
+            _stateMachine.AnimtorControler.SetTrigger("follow");
         }
         else
         {
             _stateMachine.SetState(_stateMachine.EnemyPatrollingState);
+            _stateMachine.AnimtorControler.SetTrigger("patrol");
         }
     }
 
